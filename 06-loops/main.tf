85cd5d08a35271d68422f013ac34494fcd37d0e5
saidevops7722 <saiaws7722@gmail.com>
2023-02-28 12:14:33 +0530

    servers

--- 06-loops/main.tf
+++ 06-loops/main.tf
@@ -7,9 +7,26 @@ resource "null_resource" "nothing" {
 }
 
 variable "d2" {
-  default = [0, 1, 2, 3, 4]
+  default = ["apple", "banana"]
 }
 
-resource "null_resource" "nothing" {
+resource "null_resource" "d2" {
   count = length(var.d2)
+}
+
+variable "d3" {
+  default = {
+    apple = {
+      name = "apple"
+      count = 3
+    }
+    banana = {
+      name  = "banana"
+      count = 30
+    }
+  }
+}
+
+resource "null_resource" "d3" {
+  for_each = var.d3
 }
