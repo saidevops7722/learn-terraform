c80828734e2db8b325e5462638ee951423541091
saidevops7722 <saiaws7722@gmail.com>
2023-02-28 12:35:57 +0530

    servers

---
+++ roboshop-setup/ex3/main.tf
@@ -0,0 +1,28 @@
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+resource "aws_instance" "instances" {
+  for_each = var.instances
+  ami           = data.aws_ami.ami.image_id
+  instance_type = each.value["type"]
+  vpc_security_group_ids = ["sg-07b32ef1433b68d03"]
+  tags = {
+    Name = each.value["name"]
+  }
+}
+
+variable "instances" {
+  default = {
+    catalogue = {
+      name = "catalogue"
+      type = "t3.micro"
+    }
+    user = {
+      name = "user"
+      type = "t3.small"
+    }
+  }
+}
