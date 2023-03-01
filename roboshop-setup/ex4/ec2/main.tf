e9ab9e9316506f20f26f182aec8fa6f944dc678f
saidevops7722 <saiaws7722@gmail.com>
2023-03-01 13:39:23 +0530

    servers

---
+++ roboshop-setup/ex4/ec2/main.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+resource "aws_instance" "ec2" {
+   ami = "data.aws_ami.ami.image_id"
+   instance_type = "t3.micro"
+   vpc_security_group_ids = ["sg-07b32ef1433b68d03"]
+  tags = {
+    Name = var.component
+  }
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = "Z0697752SVK7F3TOOD2N"
+  name    = "${var.component}-dev.devopsb71.site"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.ec2.private_ip]
+}
+
+variable "component" {}
