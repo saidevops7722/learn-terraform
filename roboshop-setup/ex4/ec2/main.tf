e80b5d59a116b30e70ae205118e88a120babcca6
saidevops7722 <saiaws7722@gmail.com>
2023-03-01 14:26:33 +0530

    servers

--- roboshop-setup/ex4/ec2/main.tf
+++ roboshop-setup/ex4/ec2/main.tf
@@ -7,7 +7,7 @@ data "aws_ami" "ami" {
 resource "aws_instance" "ec2" {
    ami = data.aws_ami.ami.image_id
    instance_type = "t3.micro"
-   vpc_security_group_ids = ["sg-07b32ef1433b68d03"]
+   vpc_security_group_ids = [var.sg_id]
   tags = {
     Name = var.component
   }
@@ -23,3 +23,4 @@ resource "aws_route53_record" "record" {
 
 variable "component" {}
 variable "instance_type" {}
+variable "sg_id" {}
