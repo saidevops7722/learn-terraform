d347f04016361c05da2eb8d6199ee073c41b46eb
saidevops7722 <saiaws7722@gmail.com>
2023-03-31 16:08:42 +0530

    servers

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -3,11 +3,11 @@ resource "aws_spot_instance_request" "node1" {
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-07b32ef1433b68d03"]
   wait_for_fulfillment = true
+
   tags = {
     Name = "prom-test-server"
   }
 }
-
 resource "aws_ec2_tag" "node1" {
   resource_id = aws_spot_instance_request.node1.spot_instance_id
   key         = "Name"
@@ -18,11 +18,11 @@ resource "aws_spot_instance_request" "node2" {
   ami           = "ami-0f5628e5fe90f4a45"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-07b32ef1433b68d03"]
+
   tags = {
     Name = "prom-test-node"
   }
 }
-
 resource "aws_ec2_tag" "node2" {
   resource_id = aws_spot_instance_request.node2.spot_instance_id
   key         = "Name"
