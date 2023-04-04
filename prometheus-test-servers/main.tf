5beb4a55cd9a2c6c3518f80e5ce2136975f9680a
saidevops7722 <saiaws7722@gmail.com>
2023-04-04 12:04:28 +0530

    servers

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -9,11 +9,7 @@ resource "aws_spot_instance_request" "node1" {
   }
 }
 
-resource "aws_ec2_tag" "node1" {
-  resource_id = "aws_spot_instance_request.node1.spot_instance_id"
-  key         = "Name"
-  value       = "prom-test-server"
-}
+
 
 
 
@@ -28,11 +24,6 @@ resource "aws_spot_instance_request" "node2" {
   }
 }
 
-resource "aws_ec2_tag" "node2" {
-  resource_id = "aws_spot_instance_request.node2.spot_instance_id"
-  key         = "Name"
-  value       = "prom-test-node"
-}
 
 
 
