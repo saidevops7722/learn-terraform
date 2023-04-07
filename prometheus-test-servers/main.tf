5cf4d0135aab5d4d43af6b4fcf148e892b2a466e
saidevops7722 <saiaws7722@gmail.com>
2023-04-07 18:38:29 +0530

    servers

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -1,6 +1,6 @@
 resource "aws_spot_instance_request" "node1" {
   ami           = "ami-0f5628e5fe90f4a45"
-  instance_type = "t3.micro"
+  instance_type = "t3.small"
   vpc_security_group_ids = ["sg-07b32ef1433b68d03"]
   wait_for_fulfillment = true
 
@@ -10,13 +10,9 @@ resource "aws_spot_instance_request" "node1" {
 }
 
 
-
-
-
-
 resource "aws_spot_instance_request" "node2" {
   ami           = "ami-0f5628e5fe90f4a45"
-  instance_type = "t3.micro"
+  instance_type = "t3.small"
   vpc_security_group_ids = ["sg-07b32ef1433b68d03"]
 
   tags = {
