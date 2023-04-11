be145a4366602b25ab331c620ea4da5faefb7fce
saidevops7722 <saiaws7722@gmail.com>
2023-04-11 11:00:38 +0530

    servers

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -1,6 +1,6 @@
 resource "aws_spot_instance_request" "node1" {
   ami           = "ami-0089b8e98cd95257d"
-  instance_type = "t3.small"
+  instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-07b32ef1433b68d03"]
   wait_for_fulfillment = true
   availability_zone = "us-east-1a"
@@ -11,16 +11,30 @@ resource "aws_spot_instance_request" "node1" {
 }
 
 
-//resource "aws_spot_instance_request" "node2" {
- // ami           = "ami-0f5628e5fe90f4a45"
-//  instance_type = "t3.small"
-  //vpc_security_group_ids = ["sg-07b32ef1433b68d03"]
+resource "aws_ec2_tag" "node1" {
+  resource_id = aws_spot_instance_request.node1.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-server"
+}
 
- // tags = {
- //   Name = "prom-test-node"
-  //}
-//}
 
+resource "aws_spot_instance_request" "node2" {
+  ami           = "ami-0089b8e98cd95257d"
+instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-07b32ef1433b68d03"]
+  availability_zone = "us-east-1a"
+   tags = {
+   Name = "prom-test-node"
+   }
+ }
+
+
+
+resource "aws_ec2_tag" "node2" {
+  resource_id = aws_spot_instance_request.node2.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-node"
+}
 
 
 
